b82aac05ffa854d26d673cfe5c1d3b5fab1c7e2e
Azer444 <azerrh@code.edu.az>
2022-11-21 01:11:06 +0400

    Register

---
+++ Api-Identity-JWT/Application/Repository/Migrations/20221120210742_createProductTable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class createProductTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Product",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 881, DateTimeKind.Utc).AddTicks(2644),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 768, DateTimeKind.Utc).AddTicks(4219));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Category",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 881, DateTimeKind.Utc).AddTicks(1301),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 768, DateTimeKind.Utc).AddTicks(3243));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Book",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 883, DateTimeKind.Utc).AddTicks(9396),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 770, DateTimeKind.Utc).AddTicks(6212));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Product",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 768, DateTimeKind.Utc).AddTicks(4219),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 881, DateTimeKind.Utc).AddTicks(2644));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Category",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 768, DateTimeKind.Utc).AddTicks(3243),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 881, DateTimeKind.Utc).AddTicks(1301));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Book",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 11, 20, 21, 0, 16, 770, DateTimeKind.Utc).AddTicks(6212),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 11, 20, 21, 7, 41, 883, DateTimeKind.Utc).AddTicks(9396));
+        }
+    }
+}
